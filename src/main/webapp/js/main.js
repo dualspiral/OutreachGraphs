bf3239758cf6226cdf3fed5e5bb3ada0b51a4b55
Daniel Naylor <d.naylor@hull.ac.uk>
2016-02-11 11:28:47 +0000

    Add stacked charts

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -5,26 +5,50 @@
  */
 
 var m = (function() {
-
+    var myChart;
 
     return {
+        updateData: function () {
+            myChart.data.datasets[0].data = [1, 2, 3, 4, 5, 61];
+            myChart.update();
+        },
+
         init: function() {
+            Chart.defaults.bar.stacked = true;
             var ctx = document.getElementById("chart");
-            var myChart = new Chart(ctx, {
+            myChart = new Chart(ctx, {
+                animate: true,
+                responsiveAnimationDuration: 500,
                 type: 'bar',
                 data: {
                     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
                     datasets: [{
-                        label: '# of Votes',
+                        backgroundColor: "rgba(0,0,220,0.5)",
+                        data: [12, 19, 3, 5, 2, 3]
+                    }, {
+                        backgroundColor: "rgba(220,0,0,0.5)",
                         data: [12, 19, 3, 5, 2, 3]
                     }]
                 },
                 options:{
+                    stacked: true,
                     scales:{
+                        xAxes:[{
+                           barPercentage:1,
+                           categoryPercentage:1,
+                           stacked: true,
+                           gridLines: {
+                               drawOnChartArea: false
+                           }
+                        }],
                         yAxes:[{
-                            ticks:{
-                                beginAtZero:true
-                            }
+                           stacked: true,
+                           ticks:{
+                               beginAtZero:true
+                           },
+                           gridLines: {
+                               drawOnChartArea: false
+                           }
                         }]
                     }
                 }
